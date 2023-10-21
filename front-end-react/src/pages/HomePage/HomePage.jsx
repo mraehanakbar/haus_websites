b830b06838cd5b27af7525c4561844b255decf38
farhanhariss <farhanharis66@gmail.com>
2023-10-21 08:39:52 +0700

    fix: homepage responsive solved

--- front-end-react/src/pages/HomePage/HomePage.jsx
+++ front-end-react/src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,5 @@
 import { NavbarComponent, FooterComponent } from "../../components";
-// import kitkat from "../../assets/images/kitkat-drink.png";
-// import milo from "../../assets/images/cone-milo.png";
-// import thaigreen from "../../assets/images/gree-thai-tea.png";
-// import mangoyk from "../../assets/images/mango-yakult.png";
-// import clmf from "../../assets/images/choco-lava-milo-float.png";
-// import freshlemon from "../../assets/images/fresh-lemon-tea.png";
-// import thai from "../../assets/images/thai-tea.png";
-// import oreo from "../../assets/images/oreo.png";
-// import rame from "../../assets/images/rameee.png";
-// import hp from "../../assets/images/hp.png";
-// import toptop from "../../assets/images/Desktop.png";
-// import googleplay from "../../assets/images/googleplay.png";
-import google_play from "../../assets/images/googleplay.png";
+import google_play from "../../assets/images/google-play.png";
 import hp from "../../assets/images/hp.png";
 import app_order_decoration from "../../assets/images/app-order-decoration.png";
 import ilustrasi_visual from "../../assets/images/illlustrasi-visual-lifestyle.png";
@@ -40,18 +28,14 @@ import kitkat_blur_bottom_left from "../../assets/images/kitkat-blur-bottom-left
 import kitkat_blur_bottom_right from "../../assets/images/kitkat-blur-top-right.png";
 import "./HomePage.css";
 
-const app = () => {
-  window.open(
-    "https://play.google.com/store/apps/details?id=com.ibn.haus_mobile_app&hl=en-ID"
-  );
-};
 
-const Bigorder = () => {
+
+const HomePage = () => {
   return (
     <>
-
+    <NavbarComponent />
       {/* Kitkat */}
-      <body className="container w-screen bg-white-gray">
+      <body className="container w-screen">
         <div className="relative w-screen kitkat-bg h-auto lg:h-screen">
           {/* Decoration Background  */}
           <img
@@ -66,17 +50,17 @@ const Bigorder = () => {
           />
 
           {/* Kitkat Text and Logo */}
-          <div className="flex flex-row justify-center px-14 pt-5 lg:px-32">
+          <div className="flex flex-row justify-center px-14 pt-5 lg:px-52">
             <div className="w-2/4 my-auto">
               <img
-                className="w-3/5 lg:w-1/4"
+                className="w-3/5 lg:w-[30%] lg:min-width-[25%]"
                 src={haus_logo_putih}
                 alt="Haus Logo Putih"
               />
             </div>
             <div className="w-2/4 flex justify-end z-10">
               <img
-                className="w-3/5 right-0 lg:w-auto right-0"
+                className="w-1/2 right-0 lg:w-[20%] right-0"
                 src={kitkat_logo}
                 alt="Kitkat Logo"
               />
@@ -84,17 +68,17 @@ const Bigorder = () => {
           </div>
 
           {/* Kitkat Text and Picture */}
-          <div className="flex flex-col justify-center lg:flex-row mt-20 lg:mt-px">
-            <div className="flex justify-center h-2/5 my-auto lg:w-2/4 lg:justify-end">
+          <div className="flex flex-col justify-center lg:flex-row lg:mt-0 lg:h-[60%] lg:w-auto ">
+            <div className="flex justify-center my-20 lg:py-0 lg:h-auto my-auto lg:w-[40%] lg:justify-end">
               <img
-                className="kitkat-text w-11/12 lg:w-4/5"
+                className="kitkat-text lg:w-auto lg:max-w-[65%] object-contain"
                 src={kitkat_text}
                 alt="Kitkat Text"
               />
             </div>
-            <div className=" flex justify-center h-2/4 lg:w-3/5 lg:justify-start">
+            <div className=" flex justify-center lg:h-auto lg:w-[60%] lg:justify-start ">
               <img
-                className="kitkat-gelas w-full lg:w-8/12"
+                className="kitkat-gelas w-full lg:w-auto lg:max-w-[85%] object-contain"
                 src={kitkat_gelas}
                 alt="Kitkat Gelas"
               />
@@ -102,28 +86,30 @@ const Bigorder = () => {
           </div>
 
           {/* Logo Halal */}
-          <div className="flex lg:absolute right-0 justify-end my-10 lg:mb-28 lg:mt-auto">
+          <div className="flex py-[15px] lg:py-0 justify-end lg:absolute right-0 my-10 lg:mb-30 lg:m-auto lg:h-auto lg:w-1/5">
             <img
-              className="mr-24 w-1/12 lg:w-20 h-auto"
+              className="mr-24 w-[10%] lg:w-full h-auto lg:mr-32 lg:object-contain lg:max-w-[20%]"
               src={logo_halal}
               alt="Logo Halal"
             />
           </div>
 
           {/* Promo & Footer Aksen */}
-          <div className="flex flex-col mx-auto items-center justify-center pb-20 lg:absolute min-w-full lg:pb-10">
-            <div className="z-20 text-2xl text-white">Promo</div>
+          <div className="flex flex-col pb-20 lg:pb-18 lg:absolute lg:inset-x-0 bottom-0 lg:h-[28%] lg:justify-center">
+            <div className="z-20 text-2xl text-white flex justify-center lg:text-2xl">
+              Promo
+            </div>
             <img
-              className="z-10 lg:w-20"
+              className="z-10 lg:w-[4%] mx-auto "
               src={polygon_arrow}
               alt="Polygon Arrow"
             />
-            <img
-              className="footer-aksen absolute bottom-0 min-w-full lg:h-28"
-              src={footer_aksen}
-              alt="Footer Aksen"
-            />
           </div>
+          <img
+            className="footer-aksen absolute bottom-0 min-w-full lg:h-[12%]"
+            src={footer_aksen}
+            alt="Footer Aksen"
+          />
         </div>
 
         {/* Best Deal */}
@@ -167,59 +153,61 @@ const Bigorder = () => {
         </div>
 
         {/* Best Seller Menu */}
-        <div className="w-screen py-10">
+        <div className="w-screen py-32 lg:h-auto lg:flex lg:flex-col lg:justify-center lg:py-20">
+
           {/* Subtitle */}
-          <div className="flex flex-col items-center pb-10">
-            <hr className="sub-title-line mb-3.5 border-t-4 w-1/12 mx-auto my-3" />
-            <div className="sub-title text-3xl lg:text-2xl text-white font-bold">
+          <div className="flex flex-col items-center pb-6 ">
+            <hr className="sub-title-line mb-3.5 border-t-4  w-1/5 lg:w-1/12 mx-auto my-3 lg:my-0" />
+            <div className="sub-title-content mt-8 text-[#89489C] text-4xl lg:mt-5 lg:text-2xl font-bold">
               Best Seller Menu
             </div>
           </div>
 
-          <div className="flex flex-row justify-around lg:flex-col lg:mx-36">
+          <div className="flex flex-row justify-around lg:justify-evenly lg:flex-col lg:mx-auto lg:w-screen">
+
             {/* Row Pertama */}
-            <div className="flex flex-col flex-wrap items-center w-full py-4 lg:flex-row lg:justify-between ">
+            <div className="flex flex-col flex-wrap items-center w-full py-4 lg:py-2 lg:flex-row lg:justify-evenly">
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%] "
                 src={kitkat_drink}
                 alt="Kitkat Drink"
               />
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%]"
                 src={cone_milo}
                 alt="Ice Cream Cone Coklat Milo"
               />
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%]"
                 src={thai_tea}
                 alt="Thai Tea"
               />
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%]"
                 src={mango_yakult}
                 alt="Ice Mango Mix Yakult"
               />
             </div>
 
             {/* Row Kedua */}
-            <div className="flex flex-col items-center w-full py-4 lg:flex-row lg:w-full lg:justify-between">
+            <div className="flex flex-col items-center w-full py-4 lg:py-2 lg:flex-row lg:justify-evenly">
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%]"
                 src={choco_lava_milo_float}
                 alt="Choco Lava Milo Float"
               />
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%]"
                 src={fresh_lemon_tea}
                 alt="Fresh Lemon Tea"
               />
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%]"
                 src={green_thai_tea}
                 alt="Green Thai Tea"
               />
               <img
-                className="haus-menu w-2/4 py-2 lg:w-1/6"
+                className="haus-menu w-[75%] py-2 lg:py-0 lg:w-[13%]"
                 src={oreo}
                 alt="Oreo"
               />
@@ -228,26 +216,26 @@ const Bigorder = () => {
         </div>
 
         {/* Haus Rame-Rame */}
-        <div className="w-screen py-20 haus-rame ">
+        <div className="w-screen haus-rame pb-40 lg:pb-0">
           {/* Sub Title */}
-          <div className="flex flex-col items-center pb-10">
-            <hr className=" sub-title-line-rame z-10 border-t-4 w-1/12 pb-2" />
-            <div className=" text-3xl lg:text-2xl text-white font-bold">
+          <div className="flex flex-col items-center pt-20 pb-10 lg:bg-white">
+            <hr className=" sub-title-line-rame z-10 border-t-4 h-fit w-1/5 lg:w-1/12 pb-2 lg:border-pink-500"/>
+            <div className="sub-title-content font-bold mt-5 text-white text-4xl lg:mt-0 lg:text-2xl lg:text-[#89489C]">
               Haus Rame-Rame
             </div>
           </div>
 
           {/* Content */}
-          <div className="flex flex-col lg:flex-row">
+          <div className=" haus-rame flex flex-col lg:flex-row lg:py-5">
             {/* Text and Button */}
             <div className="flex flex-col items-center lg:items-start lg:order-2 lg:w-1/2 lg:my-auto">
-              <div className="text-5xl text-center text-white font-bold leading-normal w-2/4 lg:w-4/5 lg:text-left lg:text-4xl">
+              <div className="text-7xl my-10 text-center text-white font-bold leading-normal w-[80%] lg:my-0 lg:w-[75%] lg:w-4/5 lg:text-left lg:text-4xl">
                 Ciptakan Momen Bahagiamu Bersama Haus!
               </div>
-              <div className=" text-3xl text-white  py-10 w-2/4 text-center lg:text-left lg:w-4/5">
+              <div className=" text-4xl text-white py-20 w-[60%] text-center lg:py-10 lg:text-xl lg:text-left lg:w-4/5">
                 Semua menu haus! dalam paket rame-rame
               </div>
-              <button className="hidden lg:block btn-haus-rame text-white font-bold border text-xl border-white py-3 mt-10 rounded-full hover:bg-purple-600 w-5/12">
+              <button className="btn-haus-rame hidden lg:block btn-haus-rame text-white font-bold border text-xl border-white py-3 mt-10 rounded-full w-5/12">
                 Pelajari Selengkapnya
               </button>
             </div>
@@ -255,11 +243,11 @@ const Bigorder = () => {
             {/* Ilustrasi Visual Image */}
             <div className="flex flex-col items-center lg:order-1 lg:w-1/2 lg:my-auto">
               <img
-                className="w-3/5 lg:w-4/5"
+                className="w-[90%] lg:w-4/5"
                 src={ilustrasi_visual}
                 alt="Ilustrasi Visual Lifestyle"
               />
-              <button className="btn-haus-rame text-white text-3xl font-bold border border-white py-5 my-10 rounded-full hover:bg-purple-600 w-5/12 lg:hidden">
+              <button className="btn-haus-rame text-white text-4xl font-bold border border-white py-8 my-10 rounded-full w-[60%] lg:hidden">
                 Pelajari Selengkapnya
               </button>
             </div>
@@ -270,9 +258,13 @@ const Bigorder = () => {
         <div className="w-screen h-auto relative">
           {/* Background Decoration */}
           <div>
-            <img src={app_order_decoration} alt="App Haus Decoration" />
             <img
-              className="absolute bottom-0"
+              className="w-screen"
+              src={app_order_decoration}
+              alt="App Haus Decoration"
+            />
+            <img
+              className="w-screen absolute bottom-0 z-10"
               src={app_order_decoration}
               alt="App Haus Decoration"
             />
@@ -281,16 +273,14 @@ const Bigorder = () => {
           {/* Content */}
           <div className="flex flex-col lg:flex-row mx-auto">
             {/* Text & Google Play */}
-            <div className="flex flex-col items-center mx-auto lg:items-start lg:order-2 lg:w-1/2 lg:my-auto">
-              <div
-                className="haus-mobile text-6xl text-center font-bold leading-normal mx-5 lg:mx-0 lg:w-4/5 lg:text-left lg:text-4xl"
-              >
+            <div className="flex flex-col items-center mx-auto lg:items-start lg:order-2 lg:w-1/2">
+              <div className="haus-mobile text-6xl w-[90%] text-center font-bold leading-normal lg:mx-0 lg:w-4/5 lg:text-left lg:text-4xl">
                 Haus Mobile App For Order
               </div>
-              <div className=" text-4xl py-10 text-left font-bold lg:text-left lg:w-4/5 lg:text-xl lg:py-3">
+              <div className=" text-4xl w-[80%] py-10 text-left font-bold lg:text-left lg:w-4/5 lg:text-xl lg:py-3">
                 Dapatkan voucher menarik, dan ekstra promo
               </div>
-              <ul className="flex flex-col w-9/12 list-disc mb-3 text-2xl lg:w-4/5  lg:text-base lg:ms-4">
+              <ul className="flex flex-col w-[75%] list-disc mb-3 text-3xl lg:w-4/5  lg:text-base lg:ms-4">
                 <li className="mb-2 py-3 lg:py-1">
                   Lebih Mudah Pesan dengan aplikasi Haus!, kamu bisa pilih
                   pesanan Delivery, Pickup atau Dine In untuk skip antrian
@@ -299,7 +289,7 @@ const Bigorder = () => {
                   Pesan menu Haus! Favorit kamu dari Toko terdekat
                 </li>
               </ul>
-              <div className="py-7 text-3xl lg:py-2 lg:text-base">
+              <div className="py-7 text-4xl font-bold lg:py-2 lg:text-base">
                 Download aplikasi di
               </div>
               <img
@@ -311,7 +301,7 @@ const Bigorder = () => {
 
             {/* <Ilustrasi Haus App Image */}
             <div className="flex flex-col items-center lg:order-1 lg:w-1/2">
-              <img className="w-3/4 lg:w-4/5" src={hp} alt="Handphone" />
+              <img className="z-20 mt-20 w-[85%] lg:w-[50%]" src={hp} alt="Handphone" />
             </div>
           </div>
         </div>
@@ -322,4 +312,4 @@ const Bigorder = () => {
   );
 };
 
-export default Bigorder;
+export default HomePage;
