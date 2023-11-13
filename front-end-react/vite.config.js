bbde1a65b9d38873097183809d12846cb80fb75f
farhanhariss <farhanharis66@gmail.com>
2023-11-13 14:15:50 +0700

    feat: network host true

--- front-end-react/vite.config.js
+++ front-end-react/vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host:true, //show network for run localhost on mobile phone
     hmr: {
       overlay: false,
     },
