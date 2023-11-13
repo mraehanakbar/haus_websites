630e75a5b11121cf1ceea9bb663e3aa69ab9ef95
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-11-13 12:18:34 +0700

    change minuman to haus and add hover effect

--- front-end-react/src/pages/Menu/ToggleButtonGroup.jsx
+++ front-end-react/src/pages/Menu/ToggleButtonGroup.jsx
@@ -6,10 +6,10 @@ import pedes_cyin from "../../assets/icons/pedes_cyn.png";
 const ToggleButtonGroup = () => {
   return (
     <>
-      <div class="mx-auto grid w-11/12 lg:w-10/12 md:px-5 grid-cols-6 md:grid-cols-10 gap-x-3 gap-y-4 text-base sm:text-lg md:text-xl justify-center font-bold ">
+      <div class="mx-auto grid w-11/12 lg:w-10/12 md:px-5 grid-cols-6 md:grid-cols-10 gap-x-3 lg:gap-x-5 gap-y-4 text-base sm:text-lg md:text-xl justify-center font-bold ">
         <button
           x-on:click="all = true;minuman = false;roti=false; pedes_cyin=false;hot_oppa=false"
-          class=" bg-gray-button rounded-3xl text-purple-main col-span-2"
+          class=" bg-gray-button rounded-3xl text-purple-main col-span-2 hover:scale-110 hover:shadow-lg"
           x-bind:class="!all ? '' : 'bg-purple-main text-white '"
         >
           All Menu
@@ -18,16 +18,16 @@ const ToggleButtonGroup = () => {
         {/* <!-- MINUMAN --> */}
         <button
           x-on:click="all = false; minuman = true;roti=false; pedes_cyin=false;hot_oppa=false"
-          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-span-2"
+          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-span-2 hover:scale-110 hover:shadow-lg"
           x-bind:class="!minuman ? '' : 'bg-purple-main text-white'"
         >
           <img src={haus} class="w-12 scale-75 md:scale-90 lg:scale-100" />
-          <p>Minuman</p>
+          <p>Haus</p>
         </button>
         {/* <!-- GANJEL ROTI --> */}
         <button
           x-on:click="all = false; minuman=false;roti=true; pedes_cyin=false;hot_oppa=false"
-          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-span-2"
+          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-span-2 hover:scale-110 hover:shadow-lg"
           x-bind:class="!roti ? '' : 'bg-purple-main text-white'"
         >
           <img
@@ -40,7 +40,7 @@ const ToggleButtonGroup = () => {
         {/* <!-- PEDES CYIN --> */}
         <button
           x-on:click="all = false; minuman=false;roti=false; pedes_cyin=true;hot_oppa=false"
-          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-start-2 md:col-start-7 col-span-2"
+          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-start-2 md:col-start-7 col-span-2 hover:scale-110 hover:shadow-lg"
           x-bind:class="!pedes_cyin ? '' : 'bg-purple-main text-white'"
         >
           <img
@@ -53,7 +53,7 @@ const ToggleButtonGroup = () => {
         {/* <!-- HOT OPPA --> */}
         <button
           x-on:click="all = false; minuman=false;roti=false; pedes_cyin=false;hot_oppa=true"
-          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-span-2 md:col-start-9"
+          class=" py-3 md:py-4 lg:py-5 bg-gray-button rounded-3xl text-purple-main flex justify-between flex-col items-center col-span-2 md:col-start-9 hover:scale-110 hover:shadow-lg"
           x-bind:class="!hot_oppa ? '' : 'bg-purple-main text-white'"
         >
           <img src={hot_oppa} class="w-28 scale-75 md:scale-90 lg:scale-100" />
