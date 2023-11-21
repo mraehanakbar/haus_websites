d25c68d3a6657e6f3781736ca9ae9ee3c1e66c4f
Yusuf Qisthi <yqisthi2002@gmail.com>
2023-11-21 14:19:07 +0700

    change hadline menu asset

--- front-end-react/src/pages/Menu/HeadlineMenu.jsx
+++ front-end-react/src/pages/Menu/HeadlineMenu.jsx
@@ -1,14 +1,17 @@
-import { NavbarComponent, FooterComponent, ListMenu } from "../../components";
-
 import headlineImg from "../../assets/images/menu/best_seller/menu_headline.png";
-import kitkatDrink from "../../assets/images/menu/best_seller/Kitkat Drink.png";
-import chocoLava from "../../assets/images/menu/best_seller/Choco lava Milo Float.png";
-import coneMilo from "../../assets/images/menu/best_seller/cone Milo.png";
+// import kitkatDrink from "../../assets/images/menu/best_seller/Kitkat Drink.png";
+// import chocoLava from "../../assets/images/menu/best_seller/Choco lava Milo Float.png";
+// import coneMilo from "../../assets/images/menu/best_seller/cone Milo.png";
 import freshLemon from "../../assets/images/menu/best_seller/Fresh Lemon Tea.png";
 import greenThai from "../../assets/images/menu/best_seller/Green Thai Tea.png";
 import mangoYakult from "../../assets/images/menu/best_seller/Mango Yakult.png";
-import oreo from "../../assets/images/menu/best_seller/Oreo.png";
+// import oreo from "../../assets/images/menu/best_seller/Oreo.png";
 import thaiTea from "../../assets/images/menu/best_seller/thai Tea.png";
+
+import chocoCC from "../../assets/images/menu/best_seller/choco_cheese_crunchy.png";
+import chocoIceCone from "../../assets/images/menu/best_seller/choco_ice_cream_cone.png";
+import chocoLF from "../../assets/images/menu/best_seller/choco_lava_float.png";
+import cookiesNCream from "../../assets/images/menu/best_seller/cookies_n_cream.png";
 
 const HeadlineMenu = () => {
   return (
@@ -34,17 +37,17 @@ const HeadlineMenu = () => {
           <div className="mx-auto w-11/12 md:10/12 md:mt-5 lg:mt-10 bg">
             <div className="space-y-1 md:space-y-3 lg:space-y-7">
               <div className="flex justify-evenly relative">
-                <img src={kitkatDrink} className="w-1/5" />
-                <img src={coneMilo} className="w-1/5" />
+                <img src={chocoCC} className="w-1/5" />
+                <img src={chocoIceCone} className="w-1/5" />
                 <img src={thaiTea} className="w-1/5" />
                 <img src={mangoYakult} className="w-1/5" />
               </div>
 
               <div className="flex justify-evenly relative">
-                <img src={chocoLava} className="w-1/5" />
+                <img src={chocoLF} className="w-1/5" />
                 <img src={freshLemon} className="w-1/5" />
                 <img src={greenThai} className="w-1/5" />
-                <img src={oreo} className="w-1/5" />
+                <img src={cookiesNCream} className="w-1/5" />
               </div>
             </div>
           </div>
